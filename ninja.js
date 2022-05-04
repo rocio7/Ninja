8feb204b7a0bae0239c3df316166fc70f6069b8a
rocio <rocioma324@gmail.com>
2022-05-04 16:00:54 -0500

    Tarea Clase Sensei

--- ninja.js
+++ ninja.js
@@ -12,9 +12,9 @@ class Ninja{
     
     showStats(){
         console.log("Nombre: " + this.nombre);
-        console.log("Fuerza: " + this.fuerza);
-        console.log("Velocidad: " + this.velocidad);
         console.log("Salud: " + this.salud);
+        console.log("Velocidad: " + this.velocidad);
+        console.log("Fuerza: " + this.fuerza);
     }
 
     drinkSake(){
@@ -26,3 +26,26 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+console.log("*********Clase Sensei************************");
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10
+    }
+    speakWisdom(){
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        super.drinkSake();
+    }
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
